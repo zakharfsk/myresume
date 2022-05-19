945d6a11e779e0fe8d1d5be7922ed97ff2969a8f
zakharfsk <fesikzakhar@khmnu.edu.ua>
2022-05-19 19:49:31 +0300

    update Bootstrap version

--- zaharfsk/settings.py
+++ zaharfsk/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
